07b5b0453bb819086262ee80dfd40f5698cba1d9
ka3vas <tyrnis@gmail.com>
2020-07-20 13:47:26 +0200

    feature: catering page

--- src/pages/catering/catering.page.jsx
+++ src/pages/catering/catering.page.jsx
@@ -24,35 +24,39 @@ const Catering = (props) => {
 
   return (
     <div className='catering__wrapper'>
-      <h1>Catering</h1>
       <div className='display-wrapper'>
         <div className='catering__image'>
           <LazyLoadImage src={image} alt={'TOADD'} />
         </div>
         <div className='catering__text'>
-          <p>
-            Zapewniamy świeżo przygotowane i zaskakujące kompozycje sushi,
-            dostarczone zawsze na czas. Wielkość zamówienia, form i składników
-            zależą wyłącznie od Państwa, jednak zawsze pomagamy dostosować
-            propozycję do oczekiwań.
-          </p>
-          <p>Serdecznie zapraszamy do kontaktu telefonicznego.</p>
-          {restaurant ? (
-            <>
-              <a
-                className='call-btn'
-                href={`tel:${restaurant.phone.directional}${restaurant.phone.number}`}
-              >
-                <span className='btn--round'>
-                  <PhoneIcon />
-                </span>
-                <span>
-                  +{restaurant.phone.directional}{' '}
-                  {restaurant.phone.number.replace(/(.{3})/g, '$1 ')}
-                </span>
-              </a>
-            </>
-          ) : null}
+          <h1>Catering</h1>
+          <div className='wrapper'>
+            <div className='border--dash'>
+              <p>
+                Zapewniamy świeżo przygotowane i zaskakujące kompozycje sushi,
+                dostarczone zawsze na czas. Wielkość zamówienia, form i
+                składników zależą wyłącznie od Państwa, jednak zawsze pomagamy
+                dostosować propozycję do oczekiwań.
+              </p>
+              <p>Serdecznie zapraszamy do kontaktu telefonicznego.</p>
+              {restaurant ? (
+                <>
+                  <a
+                    className='call-btn'
+                    href={`tel:${restaurant.phone.directional}${restaurant.phone.number}`}
+                  >
+                    <span className='btn--round'>
+                      <PhoneIcon />
+                    </span>
+                    <span>
+                      +{restaurant.phone.directional}{' '}
+                      {restaurant.phone.number.replace(/(.{3})/g, '$1 ')}
+                    </span>
+                  </a>
+                </>
+              ) : null}
+            </div>
+          </div>
         </div>
       </div>
     </div>
