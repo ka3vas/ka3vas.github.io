a418fb851d809a8bc9009e1721c01e7c9bd11811
ka3vas <tyrnis@gmail.com>
2020-08-10 13:25:31 +0200

    remove: brodnica

--- src/assets/data/restaurants.js
+++ src/assets/data/restaurants.js
@@ -1,9 +1,9 @@
 import torun from './city/torun';
 import swiecie from './city/swiecie';
 import kwidzyn from './city/kwidzyn';
-import brodnica from './city/brodnica';
+// import brodnica from './city/brodnica';
 import chojnice from './city/chojnice';
 
-const restaurants = [torun, swiecie, kwidzyn, brodnica, chojnice];
+const restaurants = [torun, swiecie, kwidzyn, chojnice];
 
 export default restaurants;
